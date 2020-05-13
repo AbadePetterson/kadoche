c8508135e8478deae8ec3c6c99e2314245b30e05
abade <abadepetterson@gmail.com>
2020-05-13 07:58:36 -0300

    operation hours error

--- js/bootsnav.js
+++ js/bootsnav.js
@@ -190,14 +190,14 @@
             //--------------------------------------------------------------------------------/
             //Toggle show/hide operation
             //--------------------------------------------------------------------------------/
-            hide = () => {
-                var x = document.getElementById("hide");
-                if (x.style.display === "none") {
-                    x.style.display = "block";
-                } else {
-                    x.style.display = "none";
-                }
-            }
+            // = () => {
+              //  var x = document.getElementById("hide");
+                //if (x.style.display === "none") {
+                  //  x.style.display = "block";
+                //} else {
+                //    x.style.display = "none";
+                //}
+            //}
             
             // ------------------------------------------------------------------------------ //
             // Wrapper
