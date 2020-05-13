9a7cdc651d81164f67472047d46622f84d6cb63e
Abade <abadepetterson@gmail.com>
2020-05-13 11:34:48 -0300

    move to trash bootnav

--- js/main.js
+++ js/main.js
@@ -29,3 +29,13 @@ jQuery(document).ready(function ($) {
 
     //End
 });
+
+function hide() {
+    console.log('testou')
+    let x = document.getElementById("hide");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
